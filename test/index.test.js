d1af0d3c7d1e5655cf221c0b3784b454d184c753
_asdx_ <xasdx@tuta.io>
2018-01-27 14:23:33 +0000

    Reject non-object arguments

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,16 @@
-let { expect } = require("chai")
+let { expect, use } = require("chai")
+let structuredLike = require("../lib")
+
+use(structuredLike)
 
 module.exports = {
-  "hello": () => expect(true).to.be.true
+  "handles non-object arguments": {
+    "rejects primitives": () => expect(() => expect({}).to.be.structured(true)).to.throw(),
+    "rejects primitives passed to expect": () => expect(() => expect("").to.be.structured({})).to.throw(),
+    "rejects arrays": () => expect(() => expect([]).to.be.structured({})).to.throw(),
+    "rejects undefined": () => expect(() => expect(undefined).to.be.structured({})).to.throw(),
+    "rejects null": () => expect(() => expect({}).to.be.structured(null)).to.throw(),
+    "rejects functions": () => expect(() => expect({}).to.be.structured(() => {})).to.throw(),
+    "rejects Dates": () => expect(() => expect(new Date()).to.be.structured({})).to.throw()
+  }
 }
