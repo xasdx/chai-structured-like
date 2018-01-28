46a17dc4e0f090cbc0a658803b88e1e31f8a3b43
_asdx_ <xasdx@tuta.io>
2018-01-28 08:05:20 +0000

    Reject simple differences in property numbers and types

--- test/index.test.js
+++ test/index.test.js
@@ -12,5 +12,14 @@ module.exports = {
     "rejects null": () => expect(() => expect({}).to.be.structured(null)).to.throw(),
     "rejects functions": () => expect(() => expect({}).to.be.structured(() => {})).to.throw(),
     "rejects Dates": () => expect(() => expect(new Date()).to.be.structured({})).to.throw()
+  },
+  "handles identically structured objects": {
+    "accepts empty objects": () => expect({}).to.be.structured({}),
+    "compares numbers": () => expect({ n: 0 }).to.be.structured({ n: 1 })
+  },
+  "handles differences in structure": {
+    "rejects when number of properties does not match": () => expect(() => expect({ n: 0 }).to.be.structured({ n: 0, m: 0 })).to.throw(),
+    "rejects when property is missing": () => expect(() => expect({ n: 0 }).to.be.structured({ m: 0 })).to.throw(),
+    "rejects when type of the property differs": () => expect(() => expect({ n: 0 }).to.be.structured({ n: true })).to.throw()
   }
 }
