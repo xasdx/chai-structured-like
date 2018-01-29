db03edb9a6478a6f55f44a4a67562ca8ab3f2427
_asdx_ <xasdx@tuta.io>
2018-01-29 19:31:29 +0000

    Reject when actual object contains more properties than expected

--- test/index.test.js
+++ test/index.test.js
@@ -15,11 +15,14 @@ module.exports = {
   },
   "handles identically structured objects": {
     "accepts empty objects": () => expect({}).to.be.structured({}),
-    "compares numbers": () => expect({ n: 0 }).to.be.structured({ n: 1 })
+    "compares numbers": () => expect({ n: 0 }).to.be.structured({ n: 1 }),
+    "compares multiple properties": () => expect({ n: 0, m: true, s: "hi" }).to.be.structured({ n: 1, m: false, s: "hey" })
   },
   "handles differences in structure": {
     "rejects when number of properties does not match": () => expect(() => expect({ n: 0 }).to.be.structured({ n: 0, m: 0 })).to.throw(),
     "rejects when property is missing": () => expect(() => expect({ n: 0 }).to.be.structured({ m: 0 })).to.throw(),
-    "rejects when type of the property differs": () => expect(() => expect({ n: 0 }).to.be.structured({ n: true })).to.throw()
+    "rejects when type of the property differs": () => expect(() => expect({ n: 0 }).to.be.structured({ n: true })).to.throw(),
+    "rejects when properties are missing": () => expect(() => expect({ n: 0 }).to.be.structured({ n: 1, m: "hello" })).to.throw(),
+    "rejects when contains more properties": () => expect(() => expect({ n: 0, m: true }).to.be.structured({ m: false })).to.throw()
   }
 }
